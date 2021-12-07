717902d4b16ebfb5f8dfa1d91cc39f0f86d1ff39
Andrey Ponomarev <info@andreyponomarev.ru>
2021-12-07 18:31:42 +0300

    -

--- deploy-live-streamer-to-production-env.sh
+++ deploy-live-streamer-to-production-env.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-"*** Start deployment ***"
+echo "*** Start deployment ***"
 
 cd /home/andrey
 
+# Before this step you have to manually 'git clone' the repo, when you deploy for the first time
 echo "1. Clone Git repo"
-
-git clone https://github.com/ponomarevtest/node-docker-deploy.git
+git pull https://github.com/ponomarevtest/node-docker-deploy.git
 cd node-docker-deploy
-
-echo
-
-echo "2. Copy .env file from local machine to VPS"
-scp ./node/.env root@85.143.217.174:/home/andrey/node-docker-deploy/node
 
 echo
 
